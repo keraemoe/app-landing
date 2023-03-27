7f86df7d4fe96f622e8ad3369777bce5b00b33f5
keraemoe <weekera@gmail.com>
2023-03-27 15:56:58 +0600

    add

--- components/TheApp/TheApp.jsx
+++ components/TheApp/TheApp.jsx
@@ -31,7 +31,7 @@ export default function TheApp() {
                         </defs>
                     </svg>
                 </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                <div className="mx-auto max-w-7xl py-32 sm:py-48 lg:py-56">
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                             <AnimatedTextCharacter text={"Наше приложение."} />
