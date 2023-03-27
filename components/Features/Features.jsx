9237ab4310a760f12ceab5b890f5cf4b72372d2f
keraemoe <weekera@gmail.com>
2023-03-27 15:26:33 +0600

    add

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -2,31 +2,20 @@
 import AnimatedTextCharacterParagraph from '../AnimatedText/AnimatedParagraph'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
+import React, { useState } from 'react'
 
-const features = [
-    {
-        name: 'Push to deploy.',
-        description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        icon: CloudArrowUpIcon,
-    },
-    {
-        name: 'SSL certificates.',
-        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-        icon: LockClosedIcon,
-    },
-    {
-        name: 'Database backups.',
-        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-        icon: ServerIcon,
-    },
-]
 
 export default function Features() {
+    const [activeTab, setActiveTab] = useState(1);
+
+    const changeTab = (tab) => {
+        setActiveTab(tab);
+    };
+
     return (
         <div id='features' className="overflow-hidden bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div className="flex flex-wrap mx-auto justify-between gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0  lg:grid-cols-2">
                     <div className="lg:pr-8 lg:pt-4">
                         <div className="lg:max-w-lg">
                             <motion.h2
@@ -45,30 +34,110 @@ export default function Features() {
                             <h4 className="mt-6 text-lg leading-8 text-gray-600">
                                 <AnimatedTextCharacterParagraph text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,iste dolor cupiditate blanditiis ratione."} />
                             </h4>
-                            <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                                {features.map((feature) => (
-                                    <div key={feature.name} className="relative pl-9">
-                                        <motion.dt
-                                            initial="hidden"
-                                            whileInView="visible"
-                                            transition={{ duration: 0.5 }}
-                                            variants={{
-                                                visible: { opacity: 1, scale: 1, x: 0 },
-                                                hidden: { opacity: 0, scale: 1 },
-                                            }}
-                                            className="inline font-semibold text-gray-900">
-                                            <feature.icon className="absolute top-1 left-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
-                                            <AnimatedTextCharacterParagraph text={feature.name} />
-                                        </motion.dt>{' '}
-                                        <dd className="inline">
-                                            <AnimatedTextCharacterParagraph text={feature.description} />
-                                        </dd>
-                                    </div>
-                                ))}
+                            <dl className="mt-10 space-y-8 text-base leading-7 text-gray-600 ">
+                                <div className="flex flex-col pl-9 gap-6">
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(1)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 1 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Главная страница'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(2)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 2 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Объекты'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(3)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'История платежей'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(4)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Заявки'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(5)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Услуги'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(6)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Оплата за услуги'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(7)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Прфиль'} />
+                                    </motion.button>{' '}
+                                    <motion.button
+                                        initial="hidden"
+                                        whileInView="visible"
+                                        transition={{ duration: 0.5 }}
+                                        variants={{
+                                            visible: { opacity: 1, scale: 1, x: 0 },
+                                            hidden: { opacity: 0, scale: 1 },
+                                        }}
+                                        onClick={() => changeTab(8)}
+                                        className='inline font-semibold text-gray-900 ${activeTab === 3 ? "bg-gray-300" : "bg-white"}'>
+                                        <AnimatedTextCharacterParagraph text={'Привязка карты'} />
+                                    </motion.button>{' '}
+                                </div>
                             </dl>
                         </div>
-                        <motion.div
-                            className="mt-6"
+                    </div>
+                    {activeTab === 1 && (
+                        <motion.img
                             initial="hidden"
                             whileInView="visible"
                             transition={{ duration: 0.7 }}
@@ -76,28 +145,109 @@ export default function Features() {
                                 visible: { opacity: 1, scale: 1, x: 0 },
                                 hidden: { opacity: 0, scale: 0 },
                             }}
-                        >
-                            Посмотреть функционал.{' '}
-                            <Link href="/mobile" className="font-semibold text-indigo-600">
-                                <span className="absolute inset-0" aria-hidden="true" />
-                                Смотреть <span aria-hidden="true">&rarr;</span>
-                            </Link>
-                        </motion.div>
-                    </div>
-                    <motion.img
-                        initial="hidden"
-                        whileInView="visible"
-                        transition={{ duration: 0.7 }}
-                        variants={{
-                            visible: { opacity: 1, scale: 1, x: 0 },
-                            hidden: { opacity: 0, scale: 0 },
-                        }}
-                        src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                        alt="Product screenshot"
-                        className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                        width={2432}
-                        height={1442}
-                    />
+                            src="/1"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 2 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/2"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 3 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/3"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 4 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/4"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 5 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/5"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 6 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/6"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 7 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/7"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
+                    {activeTab === 8 && (
+                        <motion.img
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.7 }}
+                            variants={{
+                                visible: { opacity: 1, scale: 1, x: 0 },
+                                hidden: { opacity: 0, scale: 0 },
+                            }}
+                            src="/8"
+                            alt="Product screenshot"
+                            className=" rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[20rem] md:-ml-4 lg:-ml-0"
+                        />
+                    )}
                 </div>
             </div>
         </div>
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                     <div className="container pt-24 md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                         <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
                             <h1 className="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
-                                <AnimatedTextCharacter text={"Main Hero Message to sell your app"} />
+                                <AnimatedTextCharacter text={"Откройте для себя новые возможности Попробуйте сами. Скачайте прямо сейчас."} />
                             </h1>
                             <h4 className='leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle'>
                                 <AnimatedTextCharacterParagraph text={"Sub-hero message, not too long and not too short. Make it just right!"} />
